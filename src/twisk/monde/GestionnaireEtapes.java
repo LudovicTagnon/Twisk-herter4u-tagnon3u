649470380a65c555362f18ef78b79b6d449770e7
herter4u <herter4u@etu.univ-lorraine.fr>
2022-02-03 10:29:56 +0100

    Changement de package

---
+++ src/twisk/monde/GestionnaireEtapes.java
@@ -0,0 +1,29 @@
+package twisk.monde;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class GestionnaireEtapes implements Iterable<Etape>{
+
+    private ArrayList<Etape> etapes;
+
+    public GestionnaireEtapes(){
+
+    }
+
+    public void ajouter(Etape ... etapes){
+
+    }
+
+    public int nbEtapes(){
+        return 0;
+    }
+
+
+
+
+    @Override
+    public Iterator<Etape> iterator() {
+        return null;
+    }
+}
