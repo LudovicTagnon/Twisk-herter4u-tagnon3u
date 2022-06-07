0ba24f1b62fca22467116cd15893c4b77bee4ce8
Herter Julian <herterjulian88@gmail.com>
2022-06-07 19:37:57 +0200

    Javadoc + Readme

---
+++ src/twisk/ressources/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"twisk"},{"l":"twisk.ecouteur"},{"l":"twisk.exceptions"},{"l":"twisk.monde"},{"l":"twisk.mondeIG"},{"l":"twisk.outils"},{"l":"twisk.simulation"},{"l":"twisk.vues"}]
